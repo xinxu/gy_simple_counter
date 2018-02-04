16304c0b9b4512fdad06a9988077a1b07e560722
tassar <xinxu0x@gmail.com>
2018-02-04 16:51:09 +0800

    code polish

--- backend/counter/consumers.py
+++ backend/counter/consumers.py
@@ -8,8 +8,43 @@ class CounterConsumer(JsonWebsocketConsumer):
         self.status = 0
         self.count = 0
         self.loop = None
+    
+    def connect(self):
+        self.accept()
+        self._run_tick_loop()
 
-    async def tick(self):
+    def receive_json(self, content):
+        if content["type"] == "CLIENT/START":
+            self.status = 1
+            self._sendType("SERVER/STARTED")
+        if content["type"] == "CLIENT/PAUSE":
+            self.status = 2
+            self._sendType("SERVER/PAUSED")
+        if content["type"] == "CLIENT/RESET":
+            self.status = 0
+            self._sendType("SERVER/RESETED")
+
+    def disconnect(self):
+        self.loop.stop()
+
+    def _sendType(self, t):
+        self.send_json(
+            {
+                "type": t,
+            }
+        )
+
+    def _run_tick_loop(self):
+        def start_loop(loop):
+            asyncio.set_event_loop(loop)
+            loop.run_forever()
+        new_loop = asyncio.new_event_loop()
+        t = Thread(target=start_loop, args=(new_loop,))
+        t.start()
+        self.loop = new_loop
+        asyncio.run_coroutine_threadsafe(self._tick(), new_loop)
+
+    async def _tick(self):
         while True:
             if self.status == 1:
                 self.send_json(
@@ -23,38 +58,3 @@ class CounterConsumer(JsonWebsocketConsumer):
             await asyncio.sleep(1)
             if self.status != 0:
                 self.count += 1
-    
-    def connect(self):
-        self.accept()
-        self.run_tick_loop()
-
-    def receive_json(self, content):
-        if content["type"] == "CLIENT/START":
-            self.status = 1
-            self.sendType("SERVER/STARTED")
-        if content["type"] == "CLIENT/PAUSE":
-            self.status = 2
-            self.sendType("SERVER/PAUSED")
-        if content["type"] == "CLIENT/RESET":
-            self.status = 0
-            self.sendType("SERVER/RESETED")
-
-    def disconnect(self):
-        self.loop.stop()
-        return
-
-    def sendType(self, t):
-        self.send_json(
-            {
-                "type": t,
-            }
-        )
-    def run_tick_loop(self):
-        def start_loop(loop):
-            asyncio.set_event_loop(loop)
-            loop.run_forever()
-        new_loop = asyncio.new_event_loop()
-        t = Thread(target=start_loop, args=(new_loop,))
-        t.start()
-        self.loop = new_loop
-        asyncio.run_coroutine_threadsafe(self.tick(), new_loop)
